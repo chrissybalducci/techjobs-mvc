80032949183ada880045fb9054074f8cd93e3dc9
Chrissy Balducci <chrissy.balducci@yahoo.com>
2019-04-07 15:36:26 -0500

    Initial Commit

--- src/main/java/org/launchcode/TechjobsApplication.java
+++ src/main/java/org/launchcode/TechjobsApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TechjobsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TechjobsApplication.class, args);
+			SpringApplication.run(TechjobsApplication.class, args);
 	}
 }
--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -23,5 +23,23 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+
+        ArrayList<HashMap<String, String>> jobs;
+
+        if (searchType.equals("all") || searchTerm.equals("")){
+            jobs = JobData.findByValue(searchTerm);
+        }
+        else{
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("searchTerm", searchTerm);
+
+        return "search";
+    }
 
 }
